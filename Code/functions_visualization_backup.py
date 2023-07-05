3b6216f3203a65a05c728cf593ef5535dd25ab1e
s4nyam <jainsanyam15@stu.upes.ac.in>
2023-07-05 20:25:27 +0200

    adding a little new code for pdf outputs

--- Code/functions_visualization_backup.py
+++ Code/functions_visualization_backup.py
@@ -14,29 +14,13 @@ def plot_loss(loss_log,d1):
     #pl.plot(np.log10(loss_log), '.', alpha=0.1)
     pl.plot(loss_log, '.', alpha=0.1)
     pl.savefig('../Logs/Dynamic_Positioning/'+ str(d1) +'/Loss_Plots/loss_plot.png')
-    pl.savefig('../Logs/Dynamic_Positioning/'+ str(d1) +'/Loss_Plots/loss_plot.pdf',format='pdf')
-
     #pl.show()
 
 def plot_sample(sample,target,start):
     fig, axs = plt.subplots(nrows=1, ncols=4)
-    
-    
-    
-    # Little addition to the code by sanyam
-    plt.subplots_adjust(wspace=0.4)
-    for ax in axs:
-        # ax.set_yticks(ax.get_xticks()[::2])  # Set ticks at every second value
-        ax.tick_params(axis='x', labelsize=7)  # Change font size of x-axis coordinates
-        ax.tick_params(axis='y', labelsize=7)  # Change font size of y-axis coordinates
 
-    # Little addition to the code by sanyam
-    
-    
-    
-    
     #add title
-    fig.suptitle('start&target | chemistry | energy | alpha',y=0.35)
+    fig.suptitle('start&target | chemistry | energy | alpha')
 
     i = sample
     i[target[0],target[1],0]=1
@@ -47,13 +31,11 @@ def plot_sample(sample,target,start):
     i[:,:,2] = ((i[:,:,2] + i[:,:,2].min()) * (1/np.maximum(i[:,:,2].max(),0.00001)) * 255)
     i[:,:,1] = ((i[:,:,1] + i[:,:,1].min()) * (1/np.maximum(i[:,:,1].max(),0.00001)) * 255)
     i[:,:,0] = ((i[:,:,0] + i[:,:,0].min()) * (1/np.maximum(i[:,:,0].max(),0.00001)) * 255)
-
     #add data to plots
     axs[0].matshow(i[:,:,0])
     axs[1].matshow(i[:,:,1])
     axs[2].matshow(i[:,:,2])
     axs[3].matshow(i[:,:,3])
-    
     return fig
 
 def create_frame(t,sample,target,start,name,path):
@@ -61,8 +43,5 @@ def create_frame(t,sample,target,start,name,path):
     fig.savefig(f'../Visualization/{path}/img_{name}_{t}.png', 
                 transparent = False,  
                 facecolor = 'white'
-               )
-    fig.set_size_inches(fig.get_size_inches())  # Set figure size to match the original figure
-    fig.savefig(f'../Visualization/{path}/img_{name}_{t}.pdf'
                )
     plt.close()
